4f5665e505972a21f0a40922d334f151c62faa0b
amine-lr <146300828+amine-lr@users.noreply.github.com>
2024-09-12 21:36:27 +0200

    updating the libft.h

--- libft.h
+++ libft.h
@@ -27,4 +27,12 @@ int		ft_toupper(int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strrchr(const char *s, char c);
 char	*ft_strnstr(const char *str1, const char *str2, size_t len);
+size_t  ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t  ft_strlcat(const char *dst, const char *src, size_t dstsize);
+void    *ft_memset(void *ptr, int c, size_t len);
+void    *ft_memcpy(void *dst, const void *src, size_t n);
+void    *ft_memmove(void *dst, const void *src, size_t len);
+void    *ft_memchr(const void *s, int c, size_t n);
+int ft_memcmp(const void *s1, const void *s2, size_t n);
+
 #endif
