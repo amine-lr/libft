d5d36b8b58a1cd8c564f0199aaf26979996fef6a
amine-lr <146300828+amine-lr@users.noreply.github.com>
2024-09-12 10:26:42 +0200

    Add files via upload

---
+++ ft_strlcat.c
@@ -0,0 +1,21 @@
+#include "libft.h"
+
+size_t  ft_strlcat(const char *dst, const char *src, size_t dstsize)
+{
+    size_t  i;
+    size_t  dest_size;
+    size_t  src_size;
+
+    dest_size = ft_strlen(dst);
+    src_size = ft_strlen(src);
+    if (dstsize < = dest_size)
+        return (src_size + dstsize);
+    i = 0;
+    while (src[i] && (dest_size + i) < (dstsize - 1))
+    {
+        dst[dest_size + i] = src[i];
+        i++;
+    }
+    dst[dest_size + 1] = '\0';
+    return ( dest_size + src_size);
+}
