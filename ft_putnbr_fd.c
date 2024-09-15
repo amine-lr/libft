e63a1b450cdf5844df62c33735d1e0a829ca4f88
amine-lr <146300828+amine-lr@users.noreply.github.com>
2024-09-15 15:39:56 +0200

    done with the put functions

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,26 @@
+#include "libft.h"
+
+void ft_putchar_fd(char c, int fd)
+{
+    write(fd, &c, 1);
+}
+
+void ft_putnbr_fd(int n, int fd)
+{
+    if (n == -2147483648) 
+    {
+        ft_putchar_fd('-', fd);
+        ft_putchar_fd('2', fd);
+        n = 147483648;
+    }
+    if (n < 0)
+    {
+        ft_putchar_fd('-', fd);
+        n = -n;
+    }
+    if (n >= 10) 
+    {
+        ft_putnbr_fd(n / 10, fd);
+    }
+    ft_putchar_fd(n % 10 + '0', fd);
+}
