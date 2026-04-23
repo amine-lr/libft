3a043eda254733f731e040d362171f71d19b5b23
Amine <aminelarioui@gmail.com>
2026-04-24 00:47:55 +0200

    the newest version

--- libft.h
+++ libft.h
@@ -1,58 +1,11 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mlarioui <mlarioui@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/11 10:11:08 by mlarioui          #+#    #+#             */
-/*   Updated: 2024/09/22 04:41:03 by mlarioui         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef LIBFT_H
 # define LIBFT_H
+
 # include <stdio.h>
 # include <unistd.h>
 # include <stdlib.h>
 
-//chek funtions
-int		ft_isalnum(int c);
-int		ft_isalpha(int c);
-int		ft_isascii(int c);
-int		ft_isdigit(int c);
-int		ft_isprint(int c);
-int		ft_tolower(int c);
-int		ft_toupper(int c);
-char	*ft_strchr(const char *s, char c);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memset(void *ptr, int c, size_t len);
-void	*ft_memchr(const void *s, int c, size_t n);
-void	*ft_memmove(void *dst, const void *src, size_t len);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-void	*ft_calloc(size_t num_elements, size_t elements_size);
-size_t	ft_strlen(const char *s);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-char	*ft_strrchr(const char *s, char c);
-char	*ft_strnstr(const char *str1, const char *str2, size_t len);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
-int		ft_atoi(const char *str);
-int		ft_atoi(const char *str);
-char	*ft_strdup(const char *s1);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strjoin(char const *s1, char const *s2);
-void	ft_putchar_fd(char c, int fd);
-void	ft_putendl_fd(char *s, int fd);
-void	ft_putnbr_fd(int n, int fd);
-void	ft_putstr_fd(char *str, int fd);
-char	*ft_strtrim(char const *s1, char const *set);
-void	ft_striteri(char *s, void (*f)(unsigned int, char *));
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-char	*ft_itoa(int num);
-char	**ft_split(char const *s, char c);
-// Bonus part
+//Part3: list
 typedef struct s_list
 {
 	void				*content;
